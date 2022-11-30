51187ce93f07f3ae4bcd84c77c83a5c3eaf2d8c9
haidarfathin <haidarhaidara9@gmail.com>
2022-11-30 22:46:57 +0700

    testing new login page

--- php/add_page.php
+++ php/add_page.php
@@ -1,6 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php 
+include "config.php";
+if(isset($_POST['submit']))
+{
+    if(!isset($_FILES['gambar']['tmp_name'])){
+        echo '<span style="color:red"><b><u><i>Pilih file gambar</i></u></b></span>';
+    }
+    else
+    {
+        $file_name = $_FILES['gambar']['name'];
+        $file_size = $_FILES['gambar']['size'];
+        $file_type = $_FILES['gambar']['type'];
+        if ($file_size < 2048000 and ($file_type =='image/jpeg' or $file_type == 'image/png'))
+        {
+            $image   = addslashes(file_get_contents($_FILES['gambar']['tmp_name']));
+            $keterangan = $_POST['keterangan'];
+            $conn->query("insert into 'gambar' ('gambar','nama_gambar') values ('$image','$file_name')");
+            header("location:home_page.php");
+        }
+        else
+        {
+            echo '<span style="color:red"><b><u><i>Ukuruan File / Tipe File Tidak Sesuai</i></u></b></span>';
+        }
+    }
+}
 
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,9 +37,9 @@
 <body>
     <h1>Tambah kehilangan</h1>
     <a href="home_page.php">back</a>
-    <form action="adddata.php" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <table>
-            <tr>
+            <!-- <tr>
                 <td>Nama Barang</td>
                 <td><input type="text" name="nama_barang"></td>
             </tr>
@@ -33,10 +59,10 @@
                 <td>Kontak</td>
                 <td><input type="text" name="kontak"></input></td>
             </tr>
-            <!-- <tr>
+            <tr> -->
                 <td>Gambar</td>
                 <td><input type="file" name="gambar"></input></td>
-            </tr> -->
+            </tr>
             <tr>
                 <td></td>
                 <td><input type="submit" value="Simpan" name="submit">
